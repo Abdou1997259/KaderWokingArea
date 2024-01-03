b50efd7600d19e9ec51ed3675b2fe86f2de6cc67
Mahmoud Adly <mahmoud.adly123@hotmail.com>
2024-01-03 09:20:21 +0200

    Emplotee model

--- Kader_System.Domain/Models/HR/HrEmployee.cs
+++ Kader_System.Domain/Models/HR/HrEmployee.cs
@@ -5,70 +5,108 @@ public class HrEmployee : BaseEntity
 {
     [Key]
     public int Id { get; set; }
-    public required string Employee_name_ar { get; set; }
-    public required string Employee_name_en { get; set; }
-    public required string Father_name_ar { get; set; }
-    public required string Father_name_en { get; set; }
-    public required string Grand_father_name_ar { get; set; }
-    public required string Grand_father_name_en { get; set; }
-    public required string Family_name_ar { get; set; }
-    public required string Family_name_en { get; set; }
-    public required string Address { get; set; }
-    public required string Fixed_salary { get; set; }
-    public required DateOnly Hiring_date { get; set; }
-    public required DateOnly Immediately_date { get; set; }
-    public required double Total_salary { get; set; }
-    public required string Phone { get; set; }
-    public required string Email { get; set; }
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-    public required string Children_number { get; set; }
-    public string? Employee_image { get; set; }
-    public string? Employee_image_extension { get; set; }
+    public required string FirstNameAr { get; set; }
+    public required string FirstNameEn { get; set; }
+    public required string FatherNameAr { get; set; }
+    public required string FatherNameEn { get; set; }
+    public required string GrandFatherNameAr { get; set; }
+    public required string GrandFatherNameEn { get; set; }
+    public required string FamilyNameAr { get; set; }
+    public required string FamilyNameEn { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public string FullNameAr => $"{FirstNameAr} {FatherNameAr} {GrandFatherNameAr} {FamilyNameAr}";
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public string FullNameEn => $"{FirstNameEn} {FatherNameEn} {GrandFatherNameEn} {FamilyNameEn}";
 
-    public int Gender_id { get; set; }
-    [ForeignKey(nameof(Gender_id))]
+    public int MaritalStatusId { get; set; }
+    [ForeignKey(nameof(MaritalStatusId))]
+    public HrMaritalStatus MaritalStatus { get; set; } = default!;
+
+    public required string Address { get; set; }
+    public required double FixedSalary { get; set; }
+    public required DateOnly HiringDate { get; set; }
+    public required DateOnly ImmediatelyDate { get; set; }
+    public required bool IsActive { get; set; }
+  
+    public required double TotalSalary { get; set; }
+    public int GenderId { get; set; }
+    [ForeignKey(nameof(GenderId))]
     public HrGender Gender { get; set; } = default!;
 
-    public int Relegion_id { get; set; }
-    [ForeignKey(nameof(Relegion_id))]
-    public HrRelegion Relegion { get; set; } = default!;
+    public required DateOnly BirthDate { get; set; }
+    public int ReligionId { get; set; }
+    [ForeignKey(nameof(ReligionId))]
+    public HrRelegion Religion { get; set; } = default!;
 
-    public int SalaryPaymentWay_id { get; set; }
-    [ForeignKey(nameof(SalaryPaymentWay_id))]
+    public required string Phone { get; set; }
+    public required string Email { get; set; }
+    public  string NationalId { get; set; }
+    public string JobNumber { get; set; }
+
+    public int SalaryPaymentWayId { get; set; }
+    [ForeignKey(nameof(SalaryPaymentWayId))]
     public HrSalaryPaymentWay SalaryPaymentWay { get; set; } = default!;
 
-    public int Shift_id { get; set; }
-    [ForeignKey(nameof(Shift_id))]
-    public HrShift Shift { get; set; } = default!;
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    [DefaultValue(0)]
+    public required int ChildrenNumber { get; set; }
 
-    public int Department_id { get; set; }
-    [ForeignKey(nameof(Department_id))]
+
+
+    public int ShiftId { get; set; }
+    [ForeignKey(nameof(ShiftId))]
+    public HrShift Shift { get; set; } = default!;
+    public int CompanyId { get; set; }
+    [ForeignKey(nameof(CompanyId))]
+    public HrCompany? Company { get; set; } = default!;
+    /// <summary>
+    /// جهاز البصمه
+    /// </summary>
+    public int? FingerPrintId { get; set; }
+
+    [ForeignKey(nameof(FingerPrintId))]
+    public HrFingerPrint FingerPrint { get; set; } = default!;
+    /// <summary>
+    /// كود الموظف المسجل على جهاز البصمه
+    /// </summary>
+    public string? FingerPrintCode { get; set; }
+
+    public string? EmployeeImage { get; set; }
+    public string? EmployeeImageExtension { get; set; }
+
+    public int ManagementId { get; set; }
+    [ForeignKey(nameof(ManagementId))]
+    public HrManagement Management { get; set; } = default!;
+
+    public int DepartmentId { get; set; }
+    [ForeignKey(nameof(DepartmentId))]
     public HrDepartment Department { get; set; } = default!;
 
-    public int Nationality_id { get; set; }
-    [ForeignKey(nameof(Nationality_id))]
+    public int NationalityId { get; set; }
+    [ForeignKey(nameof(NationalityId))]
     public HrNationality Nationality { get; set; } = default!;
 
-    public int Qualification_id { get; set; }
-    [ForeignKey(nameof(Qualification_id))]
+    public int QualificationId { get; set; }
+    [ForeignKey(nameof(QualificationId))]
     public HrQualification Qualification { get; set; } = default!;
 
-    public int AccountingWay_id { get; set; }
-    [ForeignKey(nameof(AccountingWay_id))]
-    public HrSalaryCalculator AccountingWay { get; set; } = default!;
-
-    public int Vacation_id { get; set; }
-    [ForeignKey(nameof(Vacation_id))]
+    public int VacationId { get; set; }
+    [ForeignKey(nameof(VacationId))]
     public HrVacation Vacation { get; set; } = default!;
 
-    public int EmployeeType_id { get; set; }
-    [ForeignKey(nameof(EmployeeType_id))]
+    public int JobId { get; set; }
+    [ForeignKey(nameof(JobId))]
+    public HrJob Job { get; set; } = default!;
+    /// <summary>
+    /// مقيم / مواطن
+    /// </summary>
+    public int EmployeeTypeId { get; set; }
+    [ForeignKey(nameof(EmployeeTypeId))]
     public HrEmployeeType EmployeeType { get; set; } = default!;
 
-    public int Job_id { get; set; }
-    [ForeignKey(nameof(Job_id))]
-    public HrJob Job { get; set; } = default!;
     public long? AccountNo { get; set; }
     public ICollection<HrEmployeeAttachment> ListOfAttachments { get; set; } = [];
+
+
 }
