e8ea9d04cc1d5d3b1b2cb2950662f5d45fcde9ba
Mahmoud Adly <mahmoud.adly123@hotmail.com>
2024-01-03 16:52:13 +0200

    Auto Mapper Employee Profile

--- Kader_System.Api/Profiles/HrProfile.cs
+++ Kader_System.Api/Profiles/HrProfile.cs
@@ -20,5 +20,16 @@ public class HrProfile : Profile
             .ReverseMap();
 
         #endregion
+
+        #region Employee
+        CreateMap<HrEmployee, CreateEmployeeRequest>()
+            .IgnoreAllPropertiesWithAnInaccessibleSetter()
+            .ReverseMap();
+        CreateMap<HrEmployee,EmployeesData>()
+            .ForMember(dest => dest.Company, opt => opt.MapFrom(src => src.Company!.NameAr))
+            .ForMember(dest => dest.Management, opt => opt.MapFrom(src => src.Management!.NameAr));
+
+
+        #endregion
     }
 }
