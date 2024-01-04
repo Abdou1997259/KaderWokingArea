ba0f76faa8134aa90b475bf7f67caaa5746b6afe
Mahmoud Adly <mahmoud.adly123@hotmail.com>
2024-01-04 14:52:29 +0200

    Contract Repository

--- Kader_System.DataAccess/Repositories/HR/ContractRepository.cs
+++ Kader_System.DataAccess/Repositories/HR/ContractRepository.cs
@@ -1,5 +1,52 @@
-﻿namespace Kader_System.DataAccess.Repositories.HR;
+﻿using Kader_System.Domain.DTOs.Response.HR;
+
+namespace Kader_System.DataAccess.Repositories.HR;
 
 public class ContractRepository(KaderDbContext context) : BaseRepository<HrContract>(context), IContractRepository
 {
+
+    public async Task<List<ContractData>>  GetAllContractsAsync(
+        Expression<Func<HrContract, bool>> contractFilter,
+        string lang,
+        int? skip = null,
+        int? take = null)
+    {
+        var query = context.Set<HrContract>().AsNoTracking()
+            .Include(v => v.Employee)
+            .Include(v => v.ListOfAllowancesDetails)
+            .ThenInclude(d => d.Allowance)
+            .IgnoreQueryFilters()
+            .Where(contractFilter);
+
+        if (skip.HasValue)
+            query = query.Skip(skip.Value);
+        if (take.HasValue)
+            query = query.Take(take.Value);
+
+        return await query.Select(x => new ContractData()
+        {
+            Id = x.Id,
+            TotalSalary = x.TotalSalary,
+            FixedSalary = x.FixedSalary,
+            EmployeeId = x.EmployeeId,
+            EmployeeName = lang == Localization.Arabic ? x.Employee!.FullNameAr : x.Employee!.FullNameEn,
+            StartDate = x.StartDate,
+            EndDate = x.EndDate,
+            HousingAllowance = x.HousingAllowance,
+            ContractFile = ManageFilesHelper.ConvertFileToBase64(Path.Combine(GoRootPath.HRFilesPath, x.FileName)),
+            Details = x.ListOfAllowancesDetails.Select(details => new GetAllContractDetailsResponse()
+            {
+                AllowanceId = details.AllowanceId,
+                IsPercent = details.IsPercent,
+                Value = details.Value,
+                AllowanceName = lang == Localization.Arabic ? details.Allowance!.Name_ar : details.Allowance!.Name_en,
+            }
+
+            ).ToList()
+        }).ToListAsync();
+
+
+
+
+    }
 }
--- Kader_System.Domain/Interfaces/HR/IContractRepository.cs
+++ Kader_System.Domain/Interfaces/HR/IContractRepository.cs
@@ -1,5 +1,13 @@
-﻿namespace Kader_System.Domain.Interfaces.HR;
+﻿using Kader_System.Domain.DTOs.Response.HR;
+
+namespace Kader_System.Domain.Interfaces.HR;
 
 public interface IContractRepository : IBaseRepository<HrContract>
 {
+
+    Task<List<ContractData>> GetAllContractsAsync(
+        Expression<Func<HrContract, bool>> contractFilter,
+        string lang,
+        int? skip = null,
+        int? take = null);
 }
