c4bc1d5cfd41bac6d6979795722e6743ee449b97
Mahmoud Adly <mahmoud.adly123@hotmail.com>
2024-01-04 14:52:42 +0200

    Contract Model

--- Kader_System.Domain/Models/HR/HrContract.cs
+++ Kader_System.Domain/Models/HR/HrContract.cs
@@ -5,15 +5,16 @@ public class HrContract : BaseEntity
 {
     [Key]
     public int Id { get; set; }
-    public double Salary_total { get; set; }
-    public double Salary_fixed { get; set; }
-    public DateOnly Start_date { get; set; }
-    public DateOnly End_date { get; set; }
+    public double TotalSalary { get; set; }
+    public double FixedSalary { get; set; }
+    public double HousingAllowance { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
     public required string FileName { get; set; }
     public required string FileExtension { get; set; }
 
-    public int Employee_id { get; set; }
-    [ForeignKey(nameof(Employee_id))]
+    public int EmployeeId { get; set; }
+    [ForeignKey(nameof(EmployeeId))]
     public HrEmployee Employee { get; set; } = default!;
 
     public ICollection<HrContractAllowancesDetail> ListOfAllowancesDetails { get; set; } = [];
--- Kader_System.Domain/Models/HR/HrContractAllowancesDetail.cs
+++ Kader_System.Domain/Models/HR/HrContractAllowancesDetail.cs
@@ -1,21 +1,21 @@
 ﻿namespace Kader_System.Domain.Models.HR;
-
+/// <summary>
+/// بدلات العقد
+/// </summary>
 [Table("Hr_ContractAllowancesDetails")]
 public class HrContractAllowancesDetail : BaseEntity
 {
     [Key]
     public int Id { get; set; }
-    public double Value { get; set; }
+   
 
-    public int Value_type { get; set; }
-    [ForeignKey(nameof(Value_type))]
-    public HrValueType ValueType { get; set; } = default!;
-
-    public int Salary_effect_id { get; set; }
-    [ForeignKey(nameof(Salary_effect_id))]
+    public int AllowanceId { get; set; }
+    [ForeignKey(nameof(AllowanceId))]
     public HrAllowance Allowance { get; set; } = default!;
-
-    public int Contract_id { get; set; }
-    [ForeignKey(nameof(Contract_id))]
+    public double Value { get; set; }
+    public bool IsPercent { get; set; }
+    
+    public int ContractId { get; set; }
+    [ForeignKey(nameof(ContractId))]
     public HrContract Contract { get; set; } = default!;
 }
