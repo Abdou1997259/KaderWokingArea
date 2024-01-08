d8e392b4bf4420dc9e3ed748d0709cc01905bc97
Mahmoud Adly <mahmoud.adly123@hotmail.com>
2024-01-08 18:02:45 +0200

    DTO that Creat Contract rquest

--- Kader_System.Domain/DTOs/Request/HR/CreateContractRequest.cs
+++ Kader_System.Domain/DTOs/Request/HR/CreateContractRequest.cs
@@ -16,8 +16,10 @@ namespace Kader_System.Domain.DTOs.Request.HR
         [Required(ErrorMessage = Annotations.FieldIsRequired)]
         public DateOnly EndDate { get; set; }
         [Required(ErrorMessage = Annotations.FieldIsRequired)]
-      
         public string ContractFile { get; set; }
+
+        [Required(ErrorMessage = Annotations.FieldIsRequired)]
+        public string FileName { get; set; }
         public List<CreateContractDetailsRequest>? Details { get; set; } 
 
     }
@@ -28,5 +30,7 @@ namespace Kader_System.Domain.DTOs.Request.HR
         public int AllowanceId { get; set; }
         public double Value { get; set; }
         public bool IsPercent { get; set; }
+
+        public RowStatus Status { get; set; }
     }
 }
