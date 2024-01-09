56fb0ecf36ceb43d832221fe262bfa545b9d2597
Mahmoud Adly <mahmoud.adly123@hotmail.com>
2024-01-09 14:30:42 +0200

    User Claims Repository

---
+++ Kader_System.DataAccess/Repositories/Auth/UserClaimRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Kader_System.DataAccess.Repositories.Auth
+{
+    public class UserClaimRepository(KaderDbContext context) : BaseRepository<ApplicationUserClaim>(context), IUserClaimRepository
+    {
+    }
+}
