eb367c952d02f785efb91771ba087d4524176567
Mahmoud Adly <mahmoud.adly123@hotmail.com>
2024-01-21 12:09:50 +0200

    Profiles mapper

---
+++ Kader_System.Api/Profiles/TransProfile.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Kader_System.Domain.DTOs.Response.Trans;
+
+namespace Kader_System.Api.Profiles
+{
+    public class TransProfile : Profile
+    {
+        public TransProfile()
+        {
+            #region Allowance
+
+            CreateMap<TransAllowance, CreateTransAllowanceRequest>()
+                .ReverseMap();
+            CreateMap<TransactionAllowanceGetByIdResponse, TransAllowance>()
+                .ForMember(dest => dest.Add_date, opt => opt.MapFrom(src => src.AddedOn)).ReverseMap();
+
+            #endregion
+        }
+    }
+}
