9455c854bbf6f3073521052dc08dbeedfd7ee28a
Mahmoud Adly <mahmoud.adly123@hotmail.com>
2024-01-22 11:21:50 +0200

    Trans Deduction DTOs request and response

---
+++ Kader_System.Domain/DTOs/Response/Trans/GetTransDeductionById.cs
@@ -0,0 +1,18 @@
+﻿namespace Kader_System.Domain.DTOs.Response.Trans
+{
+    public class GetTransDeductionById
+    {
+        public int Id { get; set; }
+        public DateOnly ActionMonth { get; set; }
+        public DateTime? AddedOn { get; set; }
+        public int ValueTypeId { get; set; }
+        public int SalaryEffectId { get; set; }
+        public string SalaryEffect { get; set; } = default!;
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; } = default!;
+        public int DeductionId { get; set; }
+        public string DeductionName { get; set; } = default!;
+        public string? Notes { get; set; }
+        public string? AttachmentFile { get; set; }
+    }
+}
